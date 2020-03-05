99be0545a4a57bd9bf765cacd1ed94d12287ec1c
Frazor <daocamberskies@googlemail.com>
2020-03-05 13:43:21 +0000

    Updated Headers

--- MMO/User_Data/DataWindow.cpp
+++ MMO/User_Data/DataWindow.cpp
@@ -1,3 +1,22 @@
+
+/* ----------------------------------------------------/
+ *
+ * Project created by Frazor Sharp : 2020 Mar 3rd
+ *
+ *      Amber 3D - Qt C++ Version Research 0.1
+ *
+ * Contact email  : amberskies@virginmail.com
+ * Website        : www.amberskies.org.uk
+ * Twitch         : FrazorBladezSharp
+ * Youtube        : Amberskies
+ *
+ * All code is free to use as you please
+ * Please be aware of the Open Source Licence
+ * given by Qt    : doc.qt.io/qt-5/opensourcelicence.html
+ *
+ * ---------------------------------------------------*/
+
+
 #include "DataWindow.h"
 #include "ui_DataWindow.h"
 
